ac3810c539a6a16a7c9d4f9c6bdd6defdf4ee6e4
fernandofilipuzzi.utn <fernandofilipuzzi.utn@gmail.com>
2023-12-02 15:22:55 -0300

    .

--- Ej1_TaTeTi/TaTeTiWeb/Site.Mobile.Master.designer.cs
+++ Ej1_TaTeTi/TaTeTiWeb/Site.Mobile.Master.designer.cs
@@ -15,6 +15,15 @@ namespace TaTeTiWeb
     {
 
         /// <summary>
+        /// Control HeadContent.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+
+        /// <summary>
         /// Control form1.
         /// </summary>
         /// <remarks>
